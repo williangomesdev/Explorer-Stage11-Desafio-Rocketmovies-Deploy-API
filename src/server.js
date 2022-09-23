e4c10d2cfc88ff4e380696ad3b8241eab39656cc
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-09-23 12:21:09 -0300

    variveis de ambiente dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors");
 const database = require("./database/sqlite");
 const AppError = require("./utils/AppError");
@@ -32,5 +33,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
